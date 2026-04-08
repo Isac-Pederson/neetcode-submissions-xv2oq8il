aa6d8f3c67a1c007f1dd6e0effd1cae227bc84e9
Isac <51779347+Isac-Pederson@users.noreply.github.com>
2026-04-08 13:36:05 -0700

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.java
@@ -0,0 +1,55 @@
+class TrieNode{
+    TrieNode[] children;
+    boolean endOfWord;
+
+    public TrieNode(){
+        children =  new TrieNode[26];
+        endOfWord = false;
+    }
+}
+class WordDictionary {
+    TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+        for(char c : word.toCharArray()){
+            int i = c - 'a';
+            if(curr.children[i] == null){
+                curr.children[i] = new TrieNode();
+            }
+            curr = curr.children[i];
+        }
+        curr.endOfWord =  true;
+    }
+
+    public boolean search(String word) {
+        return dfs(word,0,root);
+    }
+
+    private boolean dfs(String word, int j, TrieNode root){
+        TrieNode curr = root;
+        for(int i = j; i < word.length(); i++){
+            char c =  word.charAt(i);
+            if(c == '.'){
+                for(TrieNode child : curr.children){
+                    if(child != null && dfs(word, i+1,child)){
+                        return true;
+                    }
+                }
+                return false;
+            }else{
+                int k = c - 'a';
+                if(curr.children[k] == null){
+                    return false;
+                }
+                curr =  curr.children[k];
+            }
+        }
+        return curr.endOfWord;
+    }
+
+}
